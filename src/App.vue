aba49dba82b847aa6510be3d5b6843af11e00fa3
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-23 16:28:55 +0200

    'link'

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {  onMounted } from "vue";
+function generateRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  console.log(color)
+  return color;
+}
+onMounted(() => {
+document.querySelector('body').style=`
+background-color:#${Math.floor(Math.random()*0xffffff).toString(16)} ;
+`
+document.querySelector('.field').style=`
+background-color:#${Math.floor(Math.random()*0xffffff).toString(16)} ;
+`
+document.querySelectorAll('.definedPosition').forEach((position)=>{
+  position.style=`
+  background-color:${ generateRandomColor()} ;
+  `
+})
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<div class="field">
+<div class="definedPosition" id="position1"></div>
+<div class="definedPosition" id="position2"></div>
+<div class="definedPosition" id="position3"></div>
+<div class="definedPosition" id="position4"></div>
+<div class="definedPosition" id="position5"></div>
+<div class="definedPosition" id="position6"></div>
+<div class="definedPosition" id="position7"></div>
+<div class="definedPosition" id="position8"></div>
+<div class="definedPosition" id="position9"></div>
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style >
+*{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+body{
+  width: 100vw;
+  height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
+.field{
+  width: 30vw;
+  aspect-ratio: 1;
+  position:fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+display: flex;
+flex-direction: row;
     flex-wrap: wrap;
-  }
+    justify-content: space-evenly;
+    align-content: space-around;
+}
+.definedPosition{
+  width: 8vw;
+  aspect-ratio: 1;
+  
 }
 </style>
