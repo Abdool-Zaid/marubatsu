727d669b3fa120e5b2413dd65719c7508cdd3938
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-23 21:35:34 +0200

    'check-field-entries'

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,46 @@ import {  onMounted } from "vue";
 function generateRandomColor() {
   let letters = "0123456789ABCDEF";
   let color = "#";
+  // Math.floor(Math.random()*0xffffff).toString(16)
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
-  console.log(color)
   return color;
 }
+let MoveSet=[]
+function fieldAction(id){
+ let coor=id.split('').pop()
+ let target= document.getElementById(id)
+ let Nextmove
+if(target.innerHTML){
+  alert('already made that move')
+}else{
+  // check field
+  if(!MoveSet.length || MoveSet.pop()=='O'){
+    Nextmove='X'
+  }
+  else{
+    Nextmove='O'
+  }
+  MoveSet.push(Nextmove)
+  target.innerHTML=Nextmove
+}
+  }
 onMounted(() => {
+  let position1=document.querySelector('#position1')
+  let position2=document.querySelector('#position2')
+  let position3=document.querySelector('#position3')
+  let position4=document.querySelector('#position4')
+  let position5=document.querySelector('#position5')
+  let position6=document.querySelector('#position6')
+  let position7=document.querySelector('#position7')
+  let position8=document.querySelector('#position8')
+  let position9=document.querySelector('#position9')
 document.querySelector('body').style=`
-background-color:#${Math.floor(Math.random()*0xffffff).toString(16)} ;
+background-color:${generateRandomColor()} ;
 `
 document.querySelector('.field').style=`
-background-color:#${Math.floor(Math.random()*0xffffff).toString(16)} ;
+background-color:${generateRandomColor()} ;
 `
 document.querySelectorAll('.definedPosition').forEach((position)=>{
   position.style=`
@@ -26,15 +54,15 @@ document.querySelectorAll('.definedPosition').forEach((position)=>{
 
 <template>
 <div class="field">
-<div class="definedPosition" id="position1"></div>
-<div class="definedPosition" id="position2"></div>
-<div class="definedPosition" id="position3"></div>
-<div class="definedPosition" id="position4"></div>
-<div class="definedPosition" id="position5"></div>
-<div class="definedPosition" id="position6"></div>
-<div class="definedPosition" id="position7"></div>
-<div class="definedPosition" id="position8"></div>
-<div class="definedPosition" id="position9"></div>
+<div class="definedPosition text-center" @click="fieldAction('position1')" id="position1"></div>
+<div class="definedPosition text-center" @click="fieldAction('position2')" id="position2"></div>
+<div class="definedPosition text-center" @click="fieldAction('position3')" id="position3"></div>
+<div class="definedPosition text-center" @click="fieldAction('position4')" id="position4"></div>
+<div class="definedPosition text-center" @click="fieldAction('position5')" id="position5"></div>
+<div class="definedPosition text-center" @click="fieldAction('position6')" id="position6"></div>
+<div class="definedPosition text-center" @click="fieldAction('position7')" id="position7"></div>
+<div class="definedPosition text-center" @click="fieldAction('position8')" id="position8"></div>
+<div class="definedPosition text-center" @click="fieldAction('position9')" id="position9"></div>
 </div>
 </template>
 
@@ -43,6 +71,7 @@ document.querySelectorAll('.definedPosition').forEach((position)=>{
 margin: 0;
 padding: 0;
 box-sizing: border-box;
+color: antiquewhite;
 }
 body{
   width: 100vw;
@@ -65,6 +94,5 @@ flex-direction: row;
 .definedPosition{
   width: 8vw;
   aspect-ratio: 1;
-  
 }
 </style>
